d472b96307ef899b1b1ffb678cd73736a7d805a4
Michael Trittin <michaeltrittin@gmail.com>
2015-10-23 22:14:31 -0400

    Had to redo things because stuff changed

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -2,18 +2,11 @@ from bitstring import BitString
 import sys
 import re
 
-# We will use these tuples to contain an ordered collection of registers/instructions - their index corresponds to their bit value
-REGISTERS = ("zero", "sp", "a0", "a1", "rr", "ra", "t0", "t1", "t2",
-	"t3", "t4", "t5", "t6", "t7", "t8", "pc")
-
-INSTRUCTIONS = ("add", "addi", "and", "eq", "jal", "jeq", "ori", "lui",
-	"lw", "or", "sll", "srl", "sra", "slt", "sw", "sub")
-
 # Used to hold any labels and their associated locations in the program
 labels = {}
 
 # Used to hold the translated binary until we can print it to stdout
-temp = []
+machine_code = []
 
 # Removes comments and extra whitespace from a line
 def uncomment(line):
@@ -30,6 +23,7 @@ def check_label(line, line_number):
 def unpack(line):
 	return re.findall("([^#,\s\(\)]+)", line)
 
+
 # Keeps track of the line that we're at: doesn't count labels, comments, or nil lines
 index = 0
 
@@ -43,49 +37,78 @@ for line in sys.stdin:
 		lines.append(uline)
 		index += 1
 
-# This is where the magic happens
-for line in lines:
+class Handler:
+	def __init__(self):
+		pass
+	def __getitem__(self, item):
+		return getattr(self, item)
 
-	unpacked = unpack(line)
-	
-	# How many bits we have left in the line
-	bits_left = 16
+	# This tuple corresponds to the registers available for use - their index is translated into their bit value
+	REGISTERS = ("zero", "sp", "a0", "a1", "rr", "ra", "t0", "t1", "t2",
+		"t3", "t4", "t5", "t6", "t7", "imm", "eq_reg")
 
-	# lw/sw and jql/jeq have to be reordered in order to match the other assembly commands
-	if re.match("lw|sw", line):
-		unpacked = [unpacked[0], unpacked[1], unpacked[3], unpacked[2]]
-	elif re.match("jal|jeq", line):
-		unpacked = [unpacked[0], labels[unpacked[1]]]
-	
-	# Contains the raw bits of the assembly
-	bitline = ""
+	# This dictionary holds the instructions for us and separates them by category
+	# Instruction types:
+	# 	R = | OP = 4 | REG0 = 4 | REG1 = 4 | REG2 = 4 |
+	#	I = | OP = 4 | REG0 = 4 |       IMM = 8       |
+	#	S = | OP = 4 | REG0 = 4 | REG1 = 4 | IMM = 4  |
+	#	J = | OP = 4 |            IMM = 12            |
 
-	# handles instructions, registers, labels, and immediates differently
-	while bits_left > 0:
-		if len(unpacked) == 0:
-			bitline += "0000 "
-			bits_left -= 4
-			continue
-		item = unpacked.pop(0)
-		if item in INSTRUCTIONS:
-			bitline += BitString(uint = INSTRUCTIONS.index(item), length = 4).bin + " "
-			bits_left -= 4
-		elif item in REGISTERS:
-			bitline += BitString(uint = REGISTERS.index(item), length = 4).bin + " "
-			bits_left -= 4
+	INSTRUCTIONS = {
+		"R": {"add":0,"and":2,"eq":3,"or":9,"slt":13,"sub":15, "jmp":6},
+		"I": {"seti":1,"lui":7,"sll":10,"srl":11,"sra":12},
+		"J": {"jal":4,"jeq":5},
+		"S": {"lw":8,"sw":14}
+	}
+
+	def handle_instruction(self, line):
+		unpacked = unpack(line)
+		for t in self.INSTRUCTIONS:
+			if unpacked[0] in self.INSTRUCTIONS[t]:
+				return self[t](unpacked)
+		return "xxxx xxxx xxxx xxxx"
+
+	def tobin(self, item, length):
+		if item in self.REGISTERS:
+			return BitString(uint = self.REGISTERS.index(item), length = length).bin
+		elif re.match("-", item):
+			return BitString(int = int(item), length = length).bin
+		elif item.isdigit():
+			return BitString(uint = int(item), length = length).bin
 		elif item in labels:
-			# shouldn't be necessary, but eventually we might want labels for immediate values (e.g. A = 1)
-			bitline += BitString(int = labels[item], length = bits_left).bin + " "
-			bits_left -= bits_left
+			return BitString(uint = int(labels[item]), length = length).bin
 		else:
-			bitline += BitString(int = int(item), length = bits_left).bin
-			bits_left -= bits_left
-			
-	temp.append(bitline)
+			for t in self.INSTRUCTIONS:
+				if item in self.INSTRUCTIONS[t]:
+					return BitString(uint = self.INSTRUCTIONS[t][item], length = length).bin
+
+		return "xxxx"
+
+	def R(self, unpacked):
+		bitline = ""
+		for i in (0, 3, 1, 2):
+			if i < len(unpacked):
+				bitline += self.tobin(unpacked[i], 4) + " "
+			else:
+				bitline += "0000 "
+		return str.strip(bitline)
+
+	def I(self, unpacked):
+		bitline = self.tobin(unpacked[0], 4) + " " + self.tobin(unpacked[1], 4) + " " + self.tobin(unpacked[2], 8)
+		return bitline
+	def J(self, unpacked):
+		bitline = self.tobin(unpacked[0], 4) + " " + self.tobin(unpacked[1], 12)
+		return bitline
+	def S(self, unpacked):
+		return self.R(unpacked)
+
+handler = Handler()
+for line in lines:
+	machine_code.append(handler.handle_instruction(line))
 
 # Now we just print it out
 longest = len(max(lines, key = len)) + 7
 
-for index, line in enumerate(temp):
+for index, line in enumerate(machine_code):
 	line_out = lines[index].ljust(longest) + "// " + line + "\n"
 	sys.stdout.write(line_out)
